86d944e4d14c6ab5bd7d61a21df2918e7612fc9e
Eliyhu Chitrit <eliyhuc@college.labs>
2024-02-16 13:26:32 +0200

    signup

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -4,7 +4,50 @@ import bcryptjs from 'bcryptjs';
 import jsonwebtoken from 'jsonwebtoken';
 import Account from '../models/accountModel.js';
 
-router.post('/signup', (req,res) => {});
+router.post('/signup', (req,res) => {
+    //1. Get the user credentials
+    const {firstName,lastName,email,password} = req.body;
+
+    //2. Check if email available
+    Account.findAll({where: {email:email}})
+    .then(async results => {
+        if(results.length == 0){
+            //3. Handle password hash
+            const hash = await bcryptjs.hash(password, 10);
+            //4. Generate verivication code
+            const code = Math.floor(1000 + Math.random() * 9000);
+            //5. Store the new account in db
+            Account.create({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: hash,
+                verificationCode: code,
+                isVerified: false
+            })
+            //6. Response
+            .then(account_created => {
+                return res.status(200).json({
+                    message: account_created
+                })
+            })
+            .catch(error => {
+                return res.status(500).json({
+                    message: error
+                })
+            })
+        } else {
+            return res.status(401).json({
+                message: 'Username is not available, please try another email'
+            })
+        }
+    })
+    .catch(error => {
+        return res.status(500).json({
+            message: error
+        })
+    })
+});
 
 router.post('/verify', (req,res) => {});
 
