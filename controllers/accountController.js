d5baa5014d4bef29a4b73ae1af08093fba1d9ec0
Eliyhu Chitrit <eliyhuc@college.labs>
2024-03-01 13:15:49 +0200

    Update accountController.js

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,50 +1,57 @@
 import express from "express";
 const router = express.Router();
 import bcryptjs from "bcryptjs";
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import Account from "../models/accountModel.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 /**
  * @swagger
  * components:
  *  schemas:
+ *      VerifyUserCode:
+ *        type: object
+ *        required:
+ *          - code
+ *          - email
+ *        properties:
+ *          email:
+ *            type: string
+ *            description: The user email address
+ *          code:
+ *            type: integer
+ *            description: The code to verify
  *      User:
- *          type: object
- *          required:
- *              - firstName
- *              - lastName
- *              - email
- *              - password
- *          properties:
- *              id:
- *                  type: integer
- *                  description: The auto-generated id of the user
- *              firstName:
- *                  type: string
- *                  description: The first name of the user
- *              lastName:
- *                  type: string
- *                  description: The last name of the user
- *              email:
- *                  type: string
- *                  description: The user email address
- *              password:
- *                  type: string
- *                  description: The user crypt password
- *              verificationCode:
- *                  type: integer
- *                  description: Generated code for account validation
- *              isVerified:
- *                  type: boolean
- *                  description: Get default value of false untill user validation
- *          example:
- *              id: 1
- *              firstName: Elihu
- *              lastName: Schitrit
- *              email: eli@qwamo.com
- *              password: password
- *              verificationCode: 1234
- *              isVerified: false
+ *        type: object
+ *        required:
+ *          - firstName
+ *          - lastName
+ *          - email
+ *          - password
+ *        properties:
+ *          id:
+ *            type: integer
+ *            description: The auto-generated id of the user
+ *          firstName:
+ *            type: string
+ *            description: The first name of the user
+ *          lastName:
+ *            type: string
+ *            description: The last name of the user
+ *          email:
+ *            type: string
+ *            description: The user email address
+ *          password:
+ *            type: string
+ *            description: The user crypt password
+ *          verificationCode:
+ *            type: integer
+ *            description: Generated code for account validation
+ *          isVerified:
+ *            type: boolean
+ *            description: Get default value of false untill user validation
  */
 
 /**
@@ -74,7 +81,6 @@ import Account from "../models/accountModel.js";
  *                      schema:
  *                          $ref: '#/components/schemas/User'
  */
-
 router.post("/signup", (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
@@ -130,7 +136,6 @@ router.post("/signup", (req, res) => {
  *                          items:
  *                              $ref: '#/components/schemas/User'
  */
-
 router.get("/users", (req, res) => {
   Account.findAll()
     .then((results) => {
@@ -166,7 +171,6 @@ router.get("/users", (req, res) => {
  *                      schema:
  *                          $ref: '#/components/schemas/User'
  */
-
 router.delete("/deleteAccount/:id", (req, res) => {
   const userId = req.params.id;
   Account.findByPk(userId)
@@ -183,7 +187,6 @@ router.delete("/deleteAccount/:id", (req, res) => {
       });
     });
 });
-
 
 /**
  * @swagger
@@ -212,50 +215,134 @@ router.delete("/deleteAccount/:id", (req, res) => {
  *            schema:
  *              $ref: '#/components/schemas/User'
  */
-
-router.put('/updateAccount/:id', (req, res) => {
+router.put("/updateAccount/:id", (req, res) => {
   const userId = req.params.id;
   const { firstName, lastName } = req.body;
 
   Account.findByPk(userId)
-  .then(account => {
-    if(account){
-
-      account.firstName = firstName;
-      account.lastName = lastName;
-      return account.save()
-      .then(results => {
-        return res.status(200).json({
-          message: results
+    .then((account) => {
+      if (account) {
+        account.firstName = firstName;
+        account.lastName = lastName;
+        return account.save().then((results) => {
+          return res.status(200).json({
+            message: results,
+          });
         });
-      })
-
-    } else {
-      return res.status(401).json({
-        message: 'User not found',
+      } else {
+        return res.status(401).json({
+          message: "User not found",
+        });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: error,
       });
-    }
-  })
-  .catch(error => {
-    return res.status(500).json({
-      message: error,
     });
-  })
-})
-
-
-
-
-
-
-router.post("/verify", (req, res) => {
-  //request code + email / id
-  //find the user in db
-  //check if code match existing code
-  //update to true + save
-  //response 
 });
 
-router.post("/login", (req, res) => {});
+/**
+ * @swagger
+ * /api/account/verify:
+ *  put:
+ *    summary: Verify user code
+ *    tags: [Accounts]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/VerifyUserCode'
+ *    responses:
+ *      200:
+ *        description: Verify the user code update
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ */
+router.put("/verify", (req, res) => {
+  const { code, email } = req.body;
+  Account.findAll({ where: { email: email } })
+    .then((results) => {
+      if (results.length > 0) {
+        const account = results[0];
+        if (parseInt(account.verificationCode) === parseInt(code)) {
+          account.isVerified = true;
+          return account.save().then((success) => {
+            return res.status(200).json({
+              message: success,
+            });
+          });
+        } else {
+          return res.status(401).json({
+            message: "Verification code is not match",
+          });
+        }
+      } else {
+        return res.status(401).json({
+          message: "User not found",
+        });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: error,
+      });
+    });
+});
+
+router.post("/login", (req, res) => {
+  // request > email + password
+  const { email, password } = req.body;
+  // find the user account
+  Account.findAll({ where: { email: email } })
+    .then(async (results) => {
+      if (results.length > 0) {
+        const account = results[0];
+
+        // check if user verified the code
+        if (account.isVerified) {
+          // check for password
+          const isMatch = await bcryptjs.compare(password, account.password);
+          if (isMatch) {
+
+
+            const dataToToken = {
+              id: account.id,
+              firstName: account.firstName,
+              lastName: account.lastName,
+              email: account.email
+            }
+
+            const token = await jwt.sign(dataToToken, process.env.TOKEN_KEY);
+            return res.status(200).json({
+              message: token
+            });
+
+
+          } else {
+            return res.status(401).json({
+              message: "The password is not match",
+            });
+          }
+        } else {
+          return res.status(401).json({
+            message: "Please verfy your account",
+          });
+        }
+      } else {
+        return res.status(401).json({
+          message: "User not found",
+        });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: error,
+      });
+    });
+});
 
 export default router;
